6310ec4fd2b760bf0efda5a0aa302a796e411c82
Szilard Nemeth <szilard.nemeth88@gmail.com>
2022-08-31 17:22:36 +0200

    xml_config.py: Add new config file types: ssl-server.xml / log4j.properties

--- hadoop/xml_config.py
+++ hadoop/xml_config.py
@@ -6,3 +6,5 @@ class HadoopConfigFile(enum.Enum):
     CAPACITY_SCHEDULER = "capacity-scheduler.xml"
     CORE_SITE = "core-site.xml"
     MAPRED_SITE = "mapred-site.xml"
+    SSL_SERVER = "ssl-server.xml"
+    LOG4J_PROPERTIES = "log4j.properties"
